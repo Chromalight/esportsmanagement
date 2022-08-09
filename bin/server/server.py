00e63b6ee05cfb7ce96532e301a475f3114cceaf
chromalight <kaandogan042007@gmail.com>
2022-08-09 15:20:31 +0000

    Added definitions for future reference.

--- bin/server/server.py
+++ bin/server/server.py
@@ -3,6 +3,7 @@ import operations
 import json
 from flask import Flask
 import os
+from flask import request 
 
 # After CLI/HTTP request based functions are all-working, the website will become the development focus.
 
@@ -12,4 +13,21 @@ app = Flask(__name__)
 def home():
     return 'This website is not intended to be opened in a browser.'
     
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.args.get('email')
+    password = request.args.get('password')
+    operations.loginUser(email, password)
+    # Session system will be implemented in the future.
+
+@app.route('/logout')
+def logout():
+    return # Reserved for post-session system development.
+
+
+@app.route('/manageteam')
+def manageteam():
+    return # Reserved for future usage.
+
+
 
